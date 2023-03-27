67cb0bf694f470e3579d24d1912bfa015925ea0d
PavelErichev <dustfud@yandex.ru>
2023-03-27 18:24:10 +0300

    Commit_1

---
+++ src/main/java/com/erichev/kata/spring/rest/template/RestTemplateClientApplication.java
@@ -0,0 +1,13 @@
+package com.erichev.kata.spring.rest.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTemplateClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTemplateClientApplication.class, args);
+	}
+
+}
